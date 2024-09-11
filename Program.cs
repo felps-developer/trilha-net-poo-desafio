b3b9c376a145082bc5b6a7b15f4af59fce9cf4ee
Felipe25687 <felipemofernandes@gmail.com>
2024-09-11 12:59:58 -0300

    atualizacao do poo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
+using System.Text;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphoneXr = new Iphone("8599964654", "iphoneXr", "0145601065", 32);
+Nokia nokia = new Nokia("8599964154", "Nokia 5", "5511000005", 128);
+
+iphoneXr.Ligar();
+nokia.ReceberLigacao();
+
+Console.WriteLine($"Modelo: {iphoneXr.Modelo}, IMEI: {iphoneXr.Imei}");
+Console.WriteLine($"Modelo: {nokia.Modelo}, IMEI: {nokia.Imei}");
+
+iphoneXr.InstalarAplicativo("facebook");
+nokia.InstalarAplicativo("Instagram");
+
